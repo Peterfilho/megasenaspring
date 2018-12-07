47189a760d824ac6ea28253965188b7cf4e246d6
Peterson <peterson.petio@gmail.com>
2018-12-07 19:54:35 -0200

    travis

--- src/main/java/br/edu/utfpr/megasena/controllers/ResultController.java
+++ src/main/java/br/edu/utfpr/megasena/controllers/ResultController.java
@@ -1,15 +1,9 @@
 package br.edu.utfpr.megasena.controllers;
 
-import java.util.TreeSet;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.edu.utfpr.megasena.models.GameModel;
-import br.edu.utfpr.megasena.util.Sort;
 
 
 @Controller
